5743e00cf5220b70b9bd6894dc4a53f871cd4671
Robert Taylor <rvtaylor94@gmail.com>
2022-04-21 17:50:03 -0600

    routed features of the product

--- routes/models.js
+++ routes/models.js
@@ -15,11 +15,27 @@ module.exports = {
 
   readProduct: (productId, callback) => {
 
-    var queryString = `SELECT * from products WHERE id=${productId}`;
+    var queryString = `SELECT
+                      products.id,          products.name,      products.slogan,
+                      products.description, products.category,  products.default_price,
+                      features.feature,     features.value
+                      FROM products JOIN features ON products.id=${productId} AND features.product_id=${productId}`;
 
     db.query(queryString)
       .then((res) => {
-        callback(null, res.rows);
+        var data = res.rows;
+        data[0].features = [];
+
+        for (let i = 0; i < data.length; i++) {
+          var temp = {
+            feature: data[i].feature,
+            value: data[i].value
+          }
+          data[0].features.push(temp);
+        }
+        delete data[0].feature;
+        delete data[0].value;
+        callback(null, data[0]);
       })
       .catch(err => callback(err))
   }
--- routes/routes.js
+++ routes/routes.js
@@ -3,15 +3,6 @@ var router = require('express').Router();
 
 //Connect controller methods to their corresponding routes
 router.get('/', controller.getAllProducts);
-
 router.get('/:productId', controller.getProduct);
-
-
-// router.post('/messages', controller.messages.post);
-
-// router.get('/users', controller.users.get);
-
-// router.post('/users', controller.users.post);
-
 
 module.exports = router;
